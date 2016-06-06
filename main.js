d0e00f50ace613d8692e453d07b600bb959eb8ac
Daniel Koics <koics.dani@gmail.com>
2016-06-06 22:57:31 +0200

    Add AJAX-logic, but something won't work
    * always ends up with 404
    * though, correct url is logged on console
    * when clicking on the url, Chrome truncates some characters at the end

--- main.js
+++ main.js
@@ -2,24 +2,56 @@
  * Created by KoicsD on 2016.06.06..
  */
 (function (app) {
-    window.addEventListener('load', function (event) {
-        document.querySelector('body').appendChild(createCard(
-            'Daniel',
-            'Koics',
-            'Bucharest',
-            '3124XV Bucharest,' +
-            'Deep str. 523.',
-            'koics.dani@example.com',
-            '450780132'
-        ));
-    });
+    window.addEventListener('load', function (event) { fetchData(3); });
     
     function fetchData(amount) {
-        var url = '';
+        if (typeof(amount) !== "number")
+            throw new TypeError('Type of parameter \'number\' must be \'number\'');
+        var url = 'www.filltext.com/?rows=' + amount.toString() +
+            '&firstName={firstName}' +
+            '&lastName={lastName}' +
+            '&city={city}' +
+            '&address={streetAddress}' +
+            '&email={email}' +
+            '&phone={phone}';
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function (event) { handleRequest(event.target); };
+        request.open('GET', url, true);
+        request.send();
+        console.log('HTTP-GET-request sent to:\n' + url);
     }
 
     function handleRequest(req) {
+        if (req.readyState !== 4)
+            return;
+        if (req.status !== 200) {
+            console.log('HTTP-GET-request failed with code: ' + req.status);
+            return;
+        }
+        console.log('HTTP-GET-request successful.');
+        console.log('Parsing...');
+        try {
+            parseResponse(req.responseText);
+        }
+        catch (err) {
+            console.log('An error occurred while parsing HTTP-response:\n' +
+                (!!err.stack ? err.stack : err.toString()));
+        }
+        console.log('HTTP-response parsed successfully.')
+    }
 
+    function parseResponse(strResp) {
+        var parsedResp = JSON.parse(strResp);
+        Array.prototype.forEach.call(parsedResp, function (obj, ind, arr) {
+            document.querySelector('body').appendChild(createCard(
+                obj.firstName,
+                obj.lastName,
+                obj.city,
+                obj.address,
+                obj.email,
+                obj.phone
+            ));
+        });
     }
     
     function createCard(firstName, lastName, city, address, email, phone) {
